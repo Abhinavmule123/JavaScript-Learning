29e03737d3c36fc8d775115e5d4233e678678443
Abhinav <abhinavmule123@gmail.com>
2021-05-18 18:00:58 -0700

    Eight  Day Code

---
+++ 18-05-21/abc.js
@@ -0,0 +1,22 @@
+// Array Methods - 
+// 1. push 
+// 2. unshift 
+// 3. pop
+// 4. shift 
+// 7. reverse 
+// 8.join  
+// 9. toString 
+// 10 . Array.isArray()
+// 11. fill 
+
+// 5. concat 
+// 6. indexOf 
+// 12. slice 
+// 13. splice 
+
+// 14. forEach 
+// 15. map 
+// 16.  filter 
+// 17. find
+// 18. every 
+// 19. some 
