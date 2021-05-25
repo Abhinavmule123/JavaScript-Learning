278f3115d0e2f96f42b22b0eddd231cdcd052fe3
Abhinav <abhinavmule123@gmail.com>
2021-05-24 17:00:29 -0700

    DOM INTRO

---
+++ 24-05-21/script.js
@@ -0,0 +1,3 @@
+// 1. document.getElementById("id"); -- it select the html element by using id.
+// alert(document.getElementById("heading"));
+document.getElementById("heading").innerHTML = "this is <i>innerhtml</i> text";
