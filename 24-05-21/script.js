99884c9c85752bf721905c949102e8e28656cd55
Abhinav <abhinavmule123@gmail.com>
2021-05-25 17:15:32 -0700

    JAVASCRIPT SELECTORS

--- 24-05-21/script.js
+++ 24-05-21/script.js
@@ -1,3 +1,24 @@
+// JAVASCRIPT Selectors
+
 // 1. document.getElementById("id"); -- it select the html element by using id.
 // alert(document.getElementById("heading"));
-document.getElementById("heading").innerHTML = "this is <i>innerhtml</i> text";
+// document.getElementById("heading").innerHTML = "this is <i>innerhtml</i> text";
+// let h1Tag = document.getElementById("heading");
+// let usertag = {name:"rahul",age:20};
+// console.log(usertag.name);
+// h1Tag.id = "heading1";
+// console.log(h1Tag.id);
+ 
+// 2. document.getElementsByClassName("heading"); 
+// let h1Tag =  document.getElementsByClassName("heading"); 
+// console.log(h1Tag[0]);
+
+//3 . document.getElementsByTagName("h1");
+// let h1Tag = document.getElementsByTagName("h1"); 
+// for(let i=0;i<h1Tag.length;i++){
+//     h1Tag[i].innerHTML = "this is js text";
+// }
+// h1Tag.forEach((val)=>{
+//     val.innerHTML = "this is js text";
+// });
+
