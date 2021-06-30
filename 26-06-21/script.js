d3eed03fe7dd5e19310b4ad5e824c90857da6a21
Abhinav <abhinavmule123@gmail.com>
2021-06-29 20:21:17 -0700

    Strict mode and array destructuring

---
+++ 26-06-21/script.js
@@ -0,0 +1,32 @@
+ "use strict";
+// a =10;
+// console.log(a);
+
+// Array Destructuring //ES 6 //imp
+let learn = ["javascript","abhi","2 month"];
+
+// let courseName = learn[0];
+// let tutor = learn[1];
+// let duration = learn[2];
+
+// let [courseName,tutor,duration] = learn;
+
+// let [courseName,tutor] = learn;
+
+// let [courseName,,duration] = learn;
+
+// let [courseName="js",tutor="am",duration="1 month"] = learn;
+
+// let courseName,tutor,duration;
+//   [courseName,tutor,duration] = learn;
+
+// console.log(courseName,tutor,duration);
+
+// let [a,b=40]  = [10];
+// let [a=1,b=2] = [3,5];
+// console.log(a,b);
+
+
+
+
+
